d8800155ecc2145b1c5e81c52533acde1e1b2821
ErickWG <erickjesuswg@hotmail.com>
2023-11-22 16:42:22 -0500

    cross

--- src/main/java/com/upc/practice/Controller/DetalleVentaController.java
+++ src/main/java/com/upc/practice/Controller/DetalleVentaController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/DetalleVenta")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 
 public class DetalleVentaController {
     public final DetalleVentaService detalleVentaService;
